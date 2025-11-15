174da7431413ecd3a1600cfd1e01a97322a5938f
VuVanDao <dao28901@gmail.com>
2025-11-15 16:46:51 +0700

    fix bug in form create card

---
+++ src/CustomLibraries/MyDndKitSensor.js
@@ -0,0 +1,20 @@
+import {
+  MouseSensor as DndMouseSensor,
+  TouchSensor as DndTouchSensor,
+} from "@dnd-kit/core";
+const handler = ({ nativeEvent: event }) => {
+  let cur = event.target;
+  while (cur) {
+    if (cur.dataset && cur.dataset.noDnd) {
+      return false;
+    }
+    cur = cur.parentElement;
+  }
+  return true;
+};
+export class MouseSensor extends DndMouseSensor {
+  static activators = [{ eventName: "onMouseDown", handler }];
+}
+export class TouchSensor extends DndTouchSensor {
+  static activators = [{ eventName: "onMouseStart", handler }];
+}
